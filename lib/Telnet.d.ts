4e79bb2111c931344e26e51328dfd8ff558aa1ea
Joshua Tenner <tenner.joshua@gmail.com>
2020-04-03 13:04:24 -0400

    remove lib

--- lib/Telnet.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class Telnet {
-    private pointer;
-    constructor();
-}
-//# sourceMappingURL=Telnet.d.ts.map
--- lib/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from "./Telnet";
-//# sourceMappingURL=index.d.ts.map
