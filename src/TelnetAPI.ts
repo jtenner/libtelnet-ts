3b0b54efc50a36f8630b4d07efa5cff28b1d6ba8
Joshua Tenner <tenner.joshua@gmail.com>
2020-04-05 13:49:22 -0400

    prettier

--- src/TelnetAPI.ts
+++ src/TelnetAPI.ts
@@ -5,7 +5,11 @@ export interface TelnetAPI {
   _init(): void;
   _malloc(size: number): number;
   HEAPU8: Uint8Array;
-  _telnet_init(compatibilityTable: number, flags: TelnetFlag, userData: number): number;
+  _telnet_init(
+    compatibilityTable: number,
+    flags: TelnetFlag,
+    userData: number,
+  ): number;
   _telnet_recv(telnet: number, buffer: number, size: number): void;
   _telnet_free(telnet: number): void;
   _free(pointer: number): void;
