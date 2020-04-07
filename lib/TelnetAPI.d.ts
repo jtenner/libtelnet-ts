d0d0e84612dedc66a35a8cdd653cf18e37c0f562
Joshua Tenner <tenner.joshua@gmail.com>
2020-04-07 10:52:41 -0400

    latest build

--- lib/TelnetAPI.d.ts
+++ lib/TelnetAPI.d.ts
@@ -5,7 +5,7 @@ export interface TelnetAPI {
     _malloc(size: number): number;
     _telnet_free(telnet: number): void;
     _telnet_iac(telnet: number, cmd: TelnetCommand): void;
-    _telnet_init(compatibilityTable: number, flags: TelnetFlag, userData: number): number;
+    _telnet_init(compatibilityTable: number, flags: TelnetFlag): number;
     _telnet_negotiate(telnet: number, cmd: TelnetNegotiationCommand, option: TelnetOption): void;
     _telnet_recv(telnet: number, buffer: number, size: number): void;
     _telnet_send_text(telnet: number, buffer: number, length: number): void;
